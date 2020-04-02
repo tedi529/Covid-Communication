5cfefec1e80e452ce97655ea177b8040167e7977
Jacob Spear <spearjm77@gmail.com>
2020-04-02 19:49:38 -0400

    Fixes schema.sql

--- analysis/schema.sql
+++ analysis/schema.sql
@@ -1,3 +1,5 @@
+
+-- Governors Table (Run with Jupyter Upload)
 CREATE TABLE governors (governor_id SERIAL PRIMARY KEY,
                         governor VARCHAR(50),
 						state VARCHAR(20),
@@ -8,14 +10,23 @@ CREATE TABLE governors (governor_id SERIAL PRIMARY KEY,
 						term_limit VARCHAR(10));
 
 
-CREATE TABLE cases (update_id SERIAL PRIMARY KEY,
+
+
+-- Cases Table (Run with Jupyter Upload)
+CREATE TABLE cases (record_date VARCHAR(10),
                     governor_id INTEGER,
 					cases INTEGER,
 					deaths INTEGER);
 ALTER TABLE cases ADD CONSTRAINT fk_governor_id FOREIGN KEY (governor_id)
 REFERENCES governors(governor_id);
 
+ALTER TABLE cases ADD CONSTRAINT pk_cases PRIMARY KEY (record_date,governor_id);
 
+
+
+
+
+-- Governors_Twitter Table (Run with Postgres Import)
 CREATE TABLE governors_twitter (governor_id INTEGER,
 							   handle_id INTEGER,
 							   twitter_handle TEXT,
